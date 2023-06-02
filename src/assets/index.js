bfaa14c9e7a2c5f87af7c3e261bc3a2815fc695f
hirey-m <hirey.m@northeastern.edu>
2023-06-02 21:19:37 +0530

    updating website content

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,30 +7,28 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
+import jenkins from "./tech/jenkins.svg"
+import logotensorflow from "./tech/tensorflow_logo.png"
+import pythonlang from"./tech/python3.svg";
+import java from "./tech/java.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import goevals from "./company/goevals.png";
+import umum from "./company/umum.svg";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import fraud from "./fraud.jpg";
+import hrconnect from "./hrconnect.png";
+import medconnect from "./medconnect.png";
 import promptpix from "./promptpix.png";
-import sloppyjoes from "./sloppyjoes.png"
+import sloppyjoes from "./sloppyjoes.png";
 
 export {
   logo,
@@ -41,26 +39,24 @@ export {
   github,
   menu,
   close,
-  css,
+  java,
   docker,
-  figma,
   git,
   html,
-  javascript,
   mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  goevals,
+  umum,
   sloppyjoes,
   promptpix,
+  pythonlang,
+  logotensorflow,
+  jenkins,
+  medconnect,
+  hrconnect,
+  fraud,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,28 +3,26 @@ import {
     backend,
     creator,
     web,
-    javascript,
     typescript,
     html,
-    css,
+    java,
     reactjs,
-    redux,
     tailwind,
     nodejs,
     mongodb,
     git,
-    figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
-    carrent,
-    jobit,
+    goevals,
+    umum,
     promptpix,
     sloppyjoes,
-    tripguide,
     threejs,
+    pythonlang,
+    logotensorflow,
+    jenkins,
+    medconnect,
+    hrconnect,
+    fraud,
   } from "../assets";
   
   export const navLinks = [
@@ -63,52 +61,52 @@ import {
   
   const technologies = [
     {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
+      name: "Java",
+      icon: java,
     },
     {
       name: "Node JS",
       icon: nodejs,
     },
     {
-      name: "MongoDB",
-      icon: mongodb,
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
     },
     {
       name: "Three JS",
       icon: threejs,
     },
     {
-      name: "git",
-      icon: git,
+      name: "HTML 5",
+      icon: html,
     },
     {
-      name: "figma",
-      icon: figma,
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name:"Python 3",
+      icon: pythonlang,
+    },
+    {
+      name: "TensorFlow",
+      icon: logotensorflow,
+    },
+    {
+      name: "Jenkins",
+      icon: jenkins,
+    },
+    {
+      name: "git",
+      icon: git,
     },
     {
       name: "docker",
@@ -118,55 +116,28 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Technology Intern",
+      company_name: "GoEvals Private Limited",
+      icon: goevals,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "Jan 2022 - June 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Delivered 100% verified, accurate, complete, and active Employee database using SQL and MS Excel",
+        "Collaborated with various business and IT teams to understand and serve their database needs",
+        "Designed & executed process improvement: optimizing data delivery, revamped infrastructure for scalability and cut down paperwork by 70%",
+        "Devised and formulated an employee performance dashboard using Tableau that facilitated a 40% increase in productivity",
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "Deep Learning Researcher",
+      company_name: "University of Mumbai",
+      icon: umum,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Aug 2021 - Nov 2021",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Worked on deep learning techniques for image classification, specifically focusing on ultrasound images to detect breast cancer",
+        "Increased the prediction accuracy by 10% by data augmentation techniques, such as image rotation, flipping, and scaling, to increase the diversity of the training data and reduce overfitting",
+        "Published in the International Journal for Research in Science Engineering & Technology (IJRSET), November Vol. 8 Issue",
       ],
     },
   ];
@@ -200,51 +171,9 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
-      description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "SloppyJoes",
-      description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-      tags: [
-        {
-          name: "express",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "nodejs",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: sloppyjoes,
-      source_code_link: "https://github.com/hirey-m/SloppyJoes",
-    },
-    {
       name: "PromptPix",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "Intoducting PromptPix: A user-friendly MERN stack-based web application driven by the OpenAI API. PromptPix uses descriptive prompts to make unique images, utilising the capabilities of artificial intelligence. Improve your creativity with AI and share your ideas with a thriving user community. With PromptPix at your disposal, you can dive into the future of picture production.",
       tags: [
         {
           name: "nodejs",
@@ -261,6 +190,90 @@ import {
       ],
       image: promptpix,
       source_code_link: "https://github.com/hirey-m/PromptPix",
+    },
+    {
+      name: "MedConnect",
+      description:
+        "A Java application that connects people to medical resources. Find doctors, book appointments, and maintain comprehensive patient histories. Empower patients and enable doctors to deliver personalized care with ease. MedConnect simplifies healthcare access and enhances medical outcomes.",
+      tags: [
+        {
+          name: "java",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "db04",
+          color: "green-text-gradient",
+        },
+        {
+          name: "apacheant",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: medconnect,
+      source_code_link: "https://github.com/hirey-m/MedConnect",
+    },
+    {
+      name: "SloppyJoes",
+      description:
+        "A real-time, full-stack food delivery web app developed using Node.js, MongoDB, and Express. It offers real-time order management for restaurants and order tracking for customers, all secured with Passport.js authentication. Experience efficient and convenient food delivery with SloppyJoes",
+      tags: [
+        {
+          name: "express",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "passport.js",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: sloppyjoes,
+      source_code_link: "https://github.com/hirey-m/SloppyJoes",
+    },
+    {
+      name: "HRConnect",
+      description:
+        " The Java Swing application for HR officers to manage employees. Perform CRUD operations, search profiles, and display employee photos. Streamline employee management with ease using HRConnect",
+      tags: [
+        {
+          name: "java",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "swing",
+          color: "green-text-gradient",
+        },
+        {
+          name: "db4o",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: hrconnect,
+      source_code_link: "https://github.com/hirey-m/HRConnect",
+    },
+    {
+      name: "Fraud Detection",
+      description:
+        " Jupyter Notebook for detecting fraud in a dataset with unbalanced samples. Trained and compared Gradient Boost, Logistic Regression, and Decision Trees using hyperparameter tuning for optimal performance. Simplify fraud detection with powerful machine learning algorithms.",
+      tags: [
+        {
+          name: "python",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "sklearn",
+          color: "green-text-gradient",
+        },
+        {
+          name: "matplotlib",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: fraud,
+      source_code_link: "https://github.com/hirey-m/FraudTransactionDetection",
     },
   ];
   
